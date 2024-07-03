5e4ec9922fa7f23a8a456a3b88e3ca1b49fdb867
Z1vran <156885631+Zokhirov@users.noreply.github.com>
2024-07-03 10:13:38 +0300

    Создание массивов и их заполнение

--- Program.cs
+++ Program.cs
@@ -10,6 +10,31 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //Зохиров
+            Console.WriteLine("Введите размерность массивов:");
+            int n = int.Parse(Console.ReadLine());
+            if (n <= 1)
+            {
+                Console.WriteLine("Ошибка! Размерность массива должна быть больше единицы");
+                return;
+            }
+
+            int[] Answer1 = new int[n];
+            int[] Answer2 = new int[n];
+
+            Console.WriteLine("Введите значенние массива Answer1:");
+            for (int i = 0; i < n; i++)
+            {
+                Answer1[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Введите значенние массива Answer2:");
+            for (int i = 0; i < n; i++)
+            {
+                Answer2[i] = int.Parse(Console.ReadLine());
+            }
+            Console.ReadKey();
         }
+    }
     }
 }
